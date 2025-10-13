ba6679bd98f715054e8c6cd0e16438de50154673
M. Price <zaptross@outlook.com>
2025-10-13 23:54:07 +1000

    chore: regenerate example

--- example/src/generated/actions_invocations.rs
+++ example/src/generated/actions_invocations.rs
@@ -2,28 +2,46 @@
 use godot::{builtin::StringName, classes::Input};
 
 pub trait InputActionInvocations {
+    /// Returns true while left_click is pressed
     fn is_fire_pressed(&self) -> bool;
+    /// Returns true when left_click is just pressed
     fn is_fire_just_pressed(&self) -> bool;
+    /// Returns true when left_click is just released
     fn is_fire_just_released(&self) -> bool;
 
+    /// Returns true while SPACE is pressed
     fn is_jump_pressed(&self) -> bool;
+    /// Returns true when SPACE is just pressed
     fn is_jump_just_pressed(&self) -> bool;
+    /// Returns true when SPACE is just released
     fn is_jump_just_released(&self) -> bool;
 
+    /// Returns true while S is pressed
     fn is_move_down_pressed(&self) -> bool;
+    /// Returns true when S is just pressed
     fn is_move_down_just_pressed(&self) -> bool;
+    /// Returns true when S is just released
     fn is_move_down_just_released(&self) -> bool;
 
+    /// Returns true while A is pressed
     fn is_move_left_pressed(&self) -> bool;
+    /// Returns true when A is just pressed
     fn is_move_left_just_pressed(&self) -> bool;
+    /// Returns true when A is just released
     fn is_move_left_just_released(&self) -> bool;
 
+    /// Returns true while D is pressed
     fn is_move_right_pressed(&self) -> bool;
+    /// Returns true when D is just pressed
     fn is_move_right_just_pressed(&self) -> bool;
+    /// Returns true when D is just released
     fn is_move_right_just_released(&self) -> bool;
 
+    /// Returns true while W is pressed
     fn is_move_up_pressed(&self) -> bool;
+    /// Returns true when W is just pressed
     fn is_move_up_just_pressed(&self) -> bool;
+    /// Returns true when W is just released
     fn is_move_up_just_released(&self) -> bool;
 }
 
