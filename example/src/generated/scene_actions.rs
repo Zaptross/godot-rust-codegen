a4d20febc20004fc07b850916318633c24b8502c
M. Price <zaptross@outlook.com>
2025-10-17 23:52:34 +1000

    chore: regenerate example

---
+++ example/src/generated/scene_actions.rs
@@ -0,0 +1,27 @@
+#[allow(dead_code)]
+use godot::{
+    prelude::Node,
+    global::Error
+};
+
+pub trait SceneActions {
+    fn change_scene_to(&self, scene_path: &str) -> Option<Error>;
+    fn change_scene_to_level_one(&self) -> Option<Error>;
+    fn change_scene_to_main(&self) -> Option<Error>;
+}
+
+impl SceneActions for Node {
+    fn change_scene_to(&self, scene_path: &str) -> Option<Error> {
+        let st = self.get_tree();
+        let mut err = None;
+
+        if st.is_some() {
+            err = Some(st.unwrap().change_scene_to_file(scene_path));
+        }
+
+        err
+    }
+
+    fn change_scene_to_level_one(&self) -> Option<Error> { self.change_scene_to("res://scenes/LevelOne.tscn"); }
+    fn change_scene_to_main(&self) -> Option<Error> { self.change_scene_to("res://scenes/Main.tscn"); }
+}
---
+++ example/src/generated/scene_consts.rs
@@ -0,0 +1,3 @@
+#[allow(dead_code)]
+pub const LEVEL_ONE: &'static str = "res://scenes/LevelOne.tscn";
+pub const MAIN: &'static str = "res://scenes/Main.tscn";
