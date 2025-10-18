93f8ba5fb1c73774b285108fdbe77823919fca3a
M. Price <26305909+Zaptross@users.noreply.github.com>
2025-10-18 13:28:11 +1000

    feat: add str const generation for actions (#6)

--- example/src/generated/actions_consts.rs
+++ example/src/generated/actions_consts.rs
@@ -4,13 +4,25 @@ use godot::builtin::StringName;
 
 /// Maps to: `left_click` or `J`
 pub fn FIRE() -> StringName { StringName::from("Fire") }
+/// Maps to: `left_click` or `J`
+pub const FIRE_STR: &'static str = "Fire";
 /// Maps to: `SPACE`
 pub fn JUMP() -> StringName { StringName::from("jump") }
+/// Maps to: `SPACE`
+pub const JUMP_STR: &'static str = "jump";
 /// Maps to: `S`
 pub fn MOVE_DOWN() -> StringName { StringName::from("move_down") }
+/// Maps to: `S`
+pub const MOVE_DOWN_STR: &'static str = "move_down";
 /// Maps to: `A`
 pub fn MOVE_LEFT() -> StringName { StringName::from("move_left") }
+/// Maps to: `A`
+pub const MOVE_LEFT_STR: &'static str = "move_left";
 /// Maps to: `D`
 pub fn MOVE_RIGHT() -> StringName { StringName::from("move_right") }
+/// Maps to: `D`
+pub const MOVE_RIGHT_STR: &'static str = "move_right";
 /// Maps to: `W`
 pub fn MOVE_UP() -> StringName { StringName::from("move_up") }
+/// Maps to: `W`
+pub const MOVE_UP_STR: &'static str = "move_up";
